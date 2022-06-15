d303565ca25bc3fb4fbdea59b78ff98a4d02f4b4
slojar <52827399+slojar@users.noreply.github.com>
2022-06-15 14:58:40 +0100

    update

--- account/views.py
+++ account/views.py
@@ -84,8 +84,6 @@ class CustomerProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        print("AUTH", self.request.auth)
-        print("USER", self.request.user)
         query = CustomerSerializer(Customer.objects.get(user=request.user), context={'request': request}).data
         return Response(query)
 
