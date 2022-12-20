2169d22fccd31d8eeb32705cf3d8cdc5fb002f0a
slojararshavin <slojararshavin@gitlab.com>
2022-12-20 22:26:02 +0100

    update

--- account/views.py
+++ account/views.py
@@ -119,10 +119,7 @@ class LoginView(APIView):
 
     def post(self, request):
         version = request.data.get("build")
-        if not version:
-            return Response({"detail": "Service is unavailable, please try again later"}, status=status.HTTP_400_BAD_REQUEST)
-
-        if version < 26:
+        if not version or version < 26:
             return Response({"detail": "Please download the latest version from your store"}, status=status.HTTP_400_BAD_REQUEST)
 
         detail, success = authenticate_user(request)
