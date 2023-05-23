6d9b709b525a4367148b153eeb39e734cac674ed
slojararshavin <slojararshavin@gitlab.com>
2023-05-23 17:01:16 +0100

    update mandate signup email

--- coporate/notifications.py
+++ coporate/notifications.py
@@ -18,7 +18,8 @@ def send_email_to_bankone_mandates(bank, sender, email, subject, content):
 
 def send_username_password_to_mandate(mandate, password):
     first_name = mandate.user.first_name
-    bank = mandate.institution.bank
+    institution = mandate.institution
+    bank = institution.bank
     bank_name = bank.name
     sender = bank.support_email
     subject = "Corporate Account (On-boarding)"
@@ -27,7 +28,9 @@ def send_username_password_to_mandate(mandate, password):
     short_name = bank.short_name
     content = f"Dear {first_name},<br><br>Please find below, your username and password for your corporate account " \
               f"opened at {bank_name}.<br><br>Username: <strong>{username}</strong><br>" \
-              f"Password: <strong>{password}</strong><br><br>Kindly change your password upon successful login.<br>" \
+              f"Password: <strong>{password}</strong><br>" \
+              f"CustomerID: <strong>{institution.customerID}</strong><br><br>" \
+              f"Kindly change your password upon successful login.<br>" \
               f"<br>Regards, <br>{bank_name} Team."
     if short_name in bank_one_banks:
         send_email_to_bankone_mandates(bank, sender, email, subject, content)
