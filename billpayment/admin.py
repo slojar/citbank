28a0d5dc06c4c565eda58e9171245045da28cfc4
slojararshavin <slojararshavin@gitlab.com>
2023-06-15 13:33:43 +0100

    update

--- billpayment/admin.py
+++ billpayment/admin.py
@@ -7,9 +7,30 @@ class BulkBillPaymentModelAdmin(admin.ModelAdmin):
     list_filter = ["checked", "verified", "approved", "status"]
 
 
+class AirtimeModeAdmin(admin.ModelAdmin):
+    list_display = ["bank", "institution", "account_no", "network", "beneficiary", "amount", "status", "created_on"]
+    list_filter = ["status"]
+
+
+class DataModeAdmin(admin.ModelAdmin):
+    list_display = ["bank", "institution", "account_no", "network", "beneficiary", "amount", "status", "created_on"]
+    list_filter = ["status"]
+
+
+class CableTVModeAdmin(admin.ModelAdmin):
+    list_display = ["bank", "institution", "account_no", "service_name", "customer_name", "smart_card_no", "amount",
+                    "status", "created_on"]
+    list_filter = ["status", "service_name"]
+
+
+class ElectricityModeAdmin(admin.ModelAdmin):
+    list_display = ["bank", "institution", "account_no", "disco_type", "meter_number", "status", "created_on"]
+    list_filter = ["status", "disco_type"]
+
+
 admin.site.register(BulkBillPayment, BulkBillPaymentModelAdmin)
-admin.site.register(Data)
-admin.site.register(Airtime)
-admin.site.register(CableTV)
-admin.site.register(Electricity)
+admin.site.register(Data, DataModeAdmin)
+admin.site.register(Airtime, AirtimeModeAdmin)
+admin.site.register(CableTV, CableTVModeAdmin)
+admin.site.register(Electricity, ElectricityModeAdmin)
 admin.site.register(BillPaymentReversal)
