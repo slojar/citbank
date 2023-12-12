f07712a9d9dc5bd1fd0468cafe474bbaeb27d623
slojararshavin <sunday@tm30.net>
2023-12-12 11:09:23 +0100

    update api

--- payattitude/api.py
+++ payattitude/api.py
@@ -25,7 +25,7 @@ def single_register(client_id, f_name, l_name, phone, m_name, bvn, accout_no):
         "Bvn": str(bvn),
         "Account": str(accout_no)
     })
-    log_request(f"LOGGING PAYATTITUDE REQUEST\nURL: {url}\nPAYLOAD: {payload}")
+    log_request(f"LOGGING PAYATTITUDE REQUEST\nURL: {url}\nHEADER: {header}\nPAYLOAD: {payload}")
     response = requests.request("POST", url=url, headers=header, data=payload)
     log_request(f"LOGGING PAYATTITUDE RESPONSE\nRESPONSE: {response.text}")
     # log_request("POST", f"url: {url}", f"header: {header}", f"payload: {payload}", f"response: {response.text}")
